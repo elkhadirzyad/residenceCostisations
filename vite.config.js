c9a6f9632fe1b80f8d154eae59ebb3aaf258ceb6
User <zyad.elkhadir@gmail.com>
2025-08-30 00:20:54 +0200

    change the route of all links in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mon-app/' // replace with your repo name or '/' if deploying to user.github.io root
+  //base: '/mon-app/' // replace with your repo name or '/' if deploying to user.github.io root
+   base: '/'
 })
